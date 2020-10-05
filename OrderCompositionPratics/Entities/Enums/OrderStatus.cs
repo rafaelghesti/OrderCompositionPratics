c52c3455c22bb455ac590cd3b74a5a6022996713
rafaelghesti <66754636+rafaelghesti@users.noreply.github.com>
2020-10-05 19:18:22 -0300

    Adicionar arquivos de projeto.

---
+++ OrderCompositionPratics/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrderCompositionPratics.Entities.Enums
+{
+    enum OrderStatus
+    {
+        PendingPayment,
+        Processing,
+        Shipped,
+        Delivered,
+    }
+}
---
+++ OrderCompositionPratics/Entities/OrderItem.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace OrderCompositionPratics.Entities
+{
+    class OrderItem
+    {
+        public int Quantify { get; set; }
+        public double Price { get; set; }
+        public Product Product { get; set; }
+
+        public OrderItem()
+        {
+
+        }
+
+        public OrderItem(int quantify, double price, Product product)
+        {
+            Quantify = quantify;
+            Price = price;
+            Product = product;
+        }
+
+        public double SubTotal()
+        {
+            return Price * Quantify;
+        }
+
+        public override string ToString()
+        {
+            return "Product: "+Product.Name
+                + ", $"
+                + Price.ToString("F2", CultureInfo.InvariantCulture)
+                + ", Quantify: "
+                + Quantify
+                + ", Subtotal: $"
+                + SubTotal().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
